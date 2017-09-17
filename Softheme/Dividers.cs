08ee91cc54645b6e12cf2c04c35631b6c164bebe
JuliaVelitchenko <julia_velitchenko@mail.ru>
2017-09-17 14:55:48 +0300

    Divide checker

---
+++ Softheme/Dividers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softheme
+{
+    class Dividers
+    {  
+        public void findDividers(){
+        try{
+            Console.Write("Enter the number:");
+            int number=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The dividers of your number are:");
+            for (int i = 1; i <= number; i++) {
+                 if (number % i == 0)
+                 {
+                     Console.Write(i+"\n");
+                 }
+           
+            }
+        }
+         catch (Exception)
+            {
+            Console.WriteLine("Error");
+            }
+        Console.ReadKey();
+    }
+    }
+}
--- Softheme/Program.cs
+++ Softheme/Program.cs
@@ -12,42 +12,11 @@ namespace Softheme
         static void Main(string[] args)
         {
             
-            int countOne = 0;
-            int [] arrOne=new int[100];//Массив,для записи количества единиц
-
-            //string text = System.IO.File.ReadAllText(@"C:\Users\Zver\Desktop\C#\Softheme\input.txt");
-            string text = System.IO.File.ReadAllText(@".\input.txt");
-            char[] arr = text.ToCharArray();
-            int j = 0;
-            for (int i = 0; i < arr.Length; i++) {
-                if (arr[i] == '1')
-                {
-                    countOne += 1;
-                }
-                else {
-                    arrOne[j] = countOne;
-                    countOne = 0;
-                    j++;
-                
-                }
-            }
-            if (countOne != 0) {
-                arrOne[j] = countOne;
-                j++;
-            }
-                
-                //Max
-            int max = 0;
-                for (int i = 0; i < j; i++) {
-                    if (arrOne[i] > max)
-                     max = arrOne[i];
-                }
-                StreamWriter sw = new StreamWriter(@".\output.txt");
-           sw.WriteLine(max);
-           sw.Close();
+           //CountOne count=new CountOne();
+            //count.countOne();
             
-            
-            
+           Dividers div=new Dividers();
+            div.findDividers();
         }
     }
 }
